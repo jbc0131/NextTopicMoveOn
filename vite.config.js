6c96f8e39e807b0070673d5297effd9619523bd8
jbc0131 <jordanbcisco@gmail.com>
2026-02-19 15:38:59 -0600

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ plugins: [react()] })
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'firebase': ['firebase/app', 'firebase/firestore'],
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+        }
+      }
+    }
+  }
+})
